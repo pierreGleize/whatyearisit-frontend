248050f5aa36ceec82594d1c45bb665a19ce8b51
Pierre Gleize <gleizepierre3gmail.com>
2024-11-01 10:39:43 +0100

    id modifier

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ fetch("https://whatyearisit-backend-omega-one.vercel.app/year")
   .then((response) => response.json())
   .then((data) => {
     console.log(data);
-    document.getElementById("title").textContent = data.year;
+    document.getElementById("year").textContent = data.year;
   });
